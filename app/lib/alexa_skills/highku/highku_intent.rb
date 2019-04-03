45ef6f1fe29f6b0a9af9e9405850b8069a98d952
Riley Thompson <rt@Rileys-MacBook-Pro.local>
2019-04-02 19:23:42 -0600

    add SyllableCounter parse wiki text blobls and returns hash with map of syllable counts

--- app/lib/alexa_skills/highku/highku_intent.rb
+++ app/lib/alexa_skills/highku/highku_intent.rb
@@ -1,5 +1,4 @@
 require_relative 'highku.rb'
-require_relative '../../concerns/html_parser.rb'
 
 module AlexaSkills::HighKu
   class Intent
@@ -37,8 +36,12 @@ module AlexaSkills::HighKu
       def process_response!
         wiki_response = fetch_wiki_response
         page_title = wiki_response[:title]
-        #classes that will strip/sanitize html, this could be added as a class method
-        # at HighKu level or into a module.
+        content = wiki_response[:content]
+        sc = AlexaSkills::HighKu::SyllableCounter.new(content)
+        binding.pry
+
+        
+        #necessary sanitazation?
         #classes to utilize syllable counting gems
         #classes to utilize csg
         #class to join the two. CoherentHaiku
